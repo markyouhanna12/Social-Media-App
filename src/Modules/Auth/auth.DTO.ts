8c049bfb150e687ac8021b66a528e6ed91e0059a
markyouhanna12 <markyouhanna1@gmail.com>
2026-06-25 12:37:41 +0300

    feat: add SignupDTO type for user registration validation

---
+++ src/Modules/Auth/auth.DTO.ts
@@ -0,0 +1,8 @@
+// DTO --> types --> validation --> nestjs DTO + validation class validators
+export type SignupDTO = {
+    firstName : string;
+    lastName :string;
+    email : string
+    password : string
+    gender : string
+}
--- src/Modules/Auth/auth.service.ts
+++ src/Modules/Auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { successResponse } from "../../Utils/response/success.response";
+import { SignupDTO } from "./auth.DTO";
 
 class AuthenticationService {
 
